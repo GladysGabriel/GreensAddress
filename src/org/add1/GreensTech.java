e10e889e17c8b5d0fc3dc54515d98290f3c74d8a
GladysGabriel <lgssujithra@gmail.com>
2023-07-31 12:59:13 -0700

    first commit

---
+++ src/org/add1/GreensTech.java
@@ -0,0 +1,31 @@
+package org.add1;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		System.out.println("aaa");
+	}
+	private void greensAdayar() {
+		System.out.println("bbb");
+	}
+	private void greensTambaram() {
+		System.out.println("ccc");
+	}
+	private void greensVelacherry() {
+		System.out.println("ddd");
+	}
+	
+	private void greensAnnaNagar() {
+		System.out.println("eee");
+	}
+	
+	public static void main (String[]args) {
+		GreensTech g= new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelacherry();
+		g.greensAnnaNagar();
+	}
+	
+}
